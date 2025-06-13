e2acd4c0ad3d8b6ac801c614928e21b6b247c40c
mlvhprasad12345 <mlvhprasad.17@gmail.com>
2025-06-13 09:20:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Example of smooth scrolling for anchor links
+document.querySelectorAll('.nav-links a').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      });
+  });
+});
